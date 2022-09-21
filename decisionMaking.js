7c012587251c3c8bd81f08c25cce2f3bafe488d3
Keenan Southall <keenan.southall@yahoo.com>
2022-09-21 11:44:50 -0600

    Add code explanations

--- decisionMaking.js
+++ decisionMaking.js
@@ -35,9 +35,38 @@ if (bearChoice === 1) {
 
 // Questions
 // 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+
+// line 12 is checking if the doorchoice is number 1, if it is then the bear clothing variable is set to hat
+// but if the door choice is any nunber except 1, then the bear clothing string is set to scarf
+
 // 2. What variable has a new value assigned to it after the first if statement executes?
+
+// bearClothing
+
 // 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+
+// scarf
+
 // 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+
+// if the door choice is equal to 1, the code executes and no other else if/else is checked, if it is not 1 then the next else if is checked
+// if the door choice is equal to 2, the code executes and no other else if/else is checked, if door choice != 2, the next condition is checked
+// if the door choice is equal to 3, the code executes and the else is not checked.
+// if the door choice is not equal to 1 2 or 3, the last else statement is ran
+// in a nutshell the code checks from top to bottom if the door choice meets any conditions, and if it doesnt, the else is ran
+// but if a condition is met before the else, that condition is ran and the code will terminate
+
+// because bear choice is set to 1, the if statement (and only the if statement) is executed
+// so the final output will be "You offer the bear your hat and the bear shows you a secret passage out!"
+
 // 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+
+// "You run as fast as you can into the next room. It's full of snakes!"
+
 // 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+
+// "You tell the bear the scarf is too small and it starts to cry!"
+
 // 7. What is your favorite ending?
+
+// "You stay with the bear and become it's best friend!"
