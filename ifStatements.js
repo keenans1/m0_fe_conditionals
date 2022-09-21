7d9732944633c85d482a3c5509d0744a9e59823c
Keenan Southall <keenan.southall@yahoo.com>
2022-09-21 14:48:48 -0600

    Add conditionals

--- ifStatements.js
+++ ifStatements.js
@@ -4,7 +4,7 @@
 // (while navigated to the codeChallenges directory):
 // `node ifStatements.js`
 
-// Example: Using the weather variable below, write code that decides 
+// Example: Using the weather variable below, write code that decides
 // what you should take with you based on the following conditions:
   // if it is sunny, print "sunscreen"
   // if it is rainy, print "umbrella"
@@ -36,7 +36,7 @@ if (weather === "sunny") {
 
 // Right now, the program will print
 // out both "I have enough money for a gumball" and
-// "I don't have enough money for a gumball". Write a 
+// "I don't have enough money for a gumball". Write a
 // conditional statement that prints only one or the other.
 
 // Experiment with manipulating the value held within numQuarters
@@ -44,13 +44,16 @@ if (weather === "sunny") {
 
 numQuarters = 0
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if (numQuarters < 2) {
+  console.log("I don't have enough money for a gumball");
+}
+else {
+  console.log("I have enough money for a gumball");
+}
 
 // #####################
 // Using the variables defined below, write code that will tell you
-// if you have the ingredients to make a pizza. A pizza requires 
+// if you have the ingredients to make a pizza. A pizza requires
 // at least two cups of flour and sauce.
 
 // You should be able to change the variables to achieve the following outconsole.log();
@@ -65,3 +68,15 @@ console.log("I don't have enough money for a gumball");
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour === 1 && hasSauce === true) {
+  console.log("I cannot make pizza");
+}
+else if (cupsOfFlour === 5 && hasSauce === false) {
+  console.log("I cannot make pizza");
+}
+else if (cupsOfFlour === 2 && hasSauce === true) {
+  console.log("I can make pizza");
+}
+else if (cupsOfFlour === 3 && hasSauce === true) {
+  console.log("I can make pizza");
+}
